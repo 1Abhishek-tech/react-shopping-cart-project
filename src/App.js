58fbead39f1d458cd59e0477fabce511bb838123
Abhishek Agnihotri <abhishekagni1234@gmail.com>
2021-08-06 19:11:45 +0530

    Add firebase database, Established read and write data in the cloud databasein the cart

--- src/App.js
+++ src/App.js
@@ -1,37 +1,86 @@
 import React from "react";
 import Cart from "./Cart";
 import Navbar from "./Navbar";
+import firebase from 'firebase';
+
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       products: [
-        {
-          title: "iPhone12 Pro Max(256GB)",
-          price: 999,
-          qty: 1,
-          img: "https://m.media-amazon.com/images/I/71MHTD3uL4L._AC_UY327_FMwebp_QL65_.jpg",
-          id: 1,
-        },
-        {
-          title: "Apple WatchSE (GPS,44mm)",
-          price: 99,
-          qty: 1,
-          img: "https://m.media-amazon.com/images/I/71rhrO49SmL._AC_UY327_FMwebp_QL65_.jpg",
-          id: 2,
-        },
-        {
-          title: "2020 Apple MacBook Pro",
-          price: 5000,
-          qty: 1,
-          img: "https://m.media-amazon.com/images/I/71jG+e7roXL._AC_UY327_FMwebp_QL65_.jpg",
-          id: 3,
-        },
+        // {
+        //   title: "iPhone12 Pro Max(256GB)",
+        //   price: 999,
+        //   qty: 1,
+        //   img: "https://m.media-amazon.com/images/I/71MHTD3uL4L._AC_UY327_FMwebp_QL65_.jpg",
+        //   id: 1,
+        // },
+        // {
+        //   title: "Apple WatchSE (GPS,44mm)",
+        //   price: 99,
+        //   qty: 1,
+        //   img: "https://m.media-amazon.com/images/I/71rhrO49SmL._AC_UY327_FMwebp_QL65_.jpg",
+        //   id: 2,
+        // },
+        // {
+        //   title: "2020 Apple MacBook Pro",
+        //   price: 5000,
+        //   qty: 1,
+        //   img: "https://m.media-amazon.com/images/I/71jG+e7roXL._AC_UY327_FMwebp_QL65_.jpg",
+        //   id: 3,
+        // },
       ],
+      loading : true,
     };
+    this.db = firebase.firestore();
     // this.increaseQuantity = this.increaseQuantity.bind(this)
     // this.testing()
+  }
+  componentDidMount(){
+    // firebase
+    // .firestore()
+    // .collection('products')
+    // .get()
+    // .then((snapshot)=>{
+    //   console.log('snapshot')
+    //   snapshot.docs.map((doc)=>{
+    //     console.log(doc.data())
+    //   })
+    //   const products = snapshot.docs.map((doc)=>{
+    //     const data = doc.data();
+    //     data['id'] = doc.id;
+        
+    //     return data;
+    //     // return doc.data;
+    //   })
+      
+    //   this.setState({
+    //     products : products,
+    //     loading: false
+    //   })
+    // })
+    this.db
+    .collection('products')
+    .onSnapshot((snapshot)=>{
+      console.log('snapshot')
+      snapshot.docs.map((doc)=>{
+        console.log(doc.data())
+      })
+      const products = snapshot.docs.map((doc)=>{
+        const data = doc.data();
+        data['id'] = doc.id;
+        
+        return data;
+        // return doc.data;
+      })
+      
+      this.setState({
+        products : products,
+        loading: false
+      })
+    })
+
   }
   handleIncreaseQuantity = (product) => {
     console.log("Increase quantity of product ", product);
@@ -77,23 +126,44 @@ class App extends React.Component {
     let cartTotal =0;
     const {products} = this.state;
     products.forEach((product)=>{
+      if(product.qty>0){
       cartTotal += product.qty*product.price 
+    }
+    return '';
     })
     return cartTotal;
   }
+  addProduct=()=>{
+    this.db
+    .collection('products')
+    .add({
+      img: '',
+      title : 'Brand New Puma Shoes',
+      price : 500,
+      qty : 1,
+    })
+    .then((docRef)=>{
+      console.log('Added Product is ', docRef)
+    })
+    .catch((error)=>{
+      console.log('Error ',error)
+    })
+  }
   render() {
-    const {products} = this.state
+    const {products, loading} = this.state
     return (
       <div className="App">
         <Navbar 
           count = {this.getCartCount()}
         />
+        <button onClick={this.addProduct} style={{padding:20, fontWeight: "bold", color: "lightgrey", backgroundColor: "black", borderRadius: 30, }}>Add a Product</button>
         <Cart
           products = {products}
           onIncreaseQuantity={this.handleIncreaseQuantity}
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProduct={this.handleDeleteProduct}
         />
+        {loading && <h1>Loading Products ..</h1>}
         <div style={{fontWeight: "bold", paddingLeft: 25}}>Total : {this.getCartTotal()}</div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import firebase from "firebase";
+import "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAgfZx7nC4igBz8TFnDlzPE4DSz3RHNcV8",
+  authDomain: "cart-f4687.firebaseapp.com",
+  projectId: "cart-f4687",
+  storageBucket: "cart-f4687.appspot.com",
+  messagingSenderId: "182126516730",
+  appId: "1:182126516730:web:a49dacbee94bb00fc22c43"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 
 ReactDOM.render(
@@ -10,4 +25,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+reportWebVitals();
 
