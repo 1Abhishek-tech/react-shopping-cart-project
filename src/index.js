424a1a2e61834ce26dc6e8bec44ae923a30336a7
Abhishek Agnihotri <abhishekagni1234@gmail.com>
2021-09-28 19:02:22 +0530

    Added firebase google analytics key

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import firebase from "firebase";
 import "firebase/firestore";
-
+import "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAgfZx7nC4igBz8TFnDlzPE4DSz3RHNcV8",
@@ -13,20 +13,20 @@ const firebaseConfig = {
   projectId: "cart-f4687",
   storageBucket: "cart-f4687.appspot.com",
   messagingSenderId: "182126516730",
-  appId: "1:182126516730:web:a49dacbee94bb00fc22c43"
+  appId: "1:182126516730:web:a49dacbee94bb00fc22c43",
+  measurementId: "G-5YBGH3JM7Z",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 reportWebVitals();
-
